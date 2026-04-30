60602ba6d63bd8f261761dc18f5f35d03441acef
wessmanj-a11y <wessman.j@gmail.com>
2026-04-30 13:26:36 -0500

    Add modular v2 config

---
+++ js/config.js
@@ -0,0 +1,8 @@
+const CONFIG = {
+  outagesUrl: './outages.json',
+  countiesUrl: 'https://raw.githubusercontent.com/plotly/datasets/master/geojson-counties-fips.json',
+  defaultMode: 'severity',
+  stateFips: '48',
+  defaultCenter: [31.05, -99.35],
+  defaultZoom: 6
+};
